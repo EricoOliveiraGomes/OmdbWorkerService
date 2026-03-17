93bedf52db620af2b20cc6a8e09bb19ffc388c65
EricoOliveiraGomes <gomeserico78@gmail.com>
2026-03-17 19:51:56 -0300

    acesso de teste à API do OMDB

--- Worker.cs
+++ Worker.cs
@@ -1,16 +1,43 @@
 namespace OmdbWorkerService
 {
-    public class Worker(ILogger<Worker> logger) : BackgroundService
+    public class Worker : BackgroundService
     {
+        private readonly ILogger<Worker> _logger;
+        private readonly HttpClient _httpClient;
+
+        public Worker(ILogger<Worker> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;            
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                if (logger.IsEnabled(LogLevel.Information))
+
+                _logger.LogInformation("Verificando API: {time}", DateTimeOffset.Now);
+
+                try {
+
+                    var response = await _httpClient.GetAsync("http://www.omdbapi.com/?i=tt3896198&apikey=b0dd6377", stoppingToken);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var conteudo = await response.Content.ReadAsStringAsync(stoppingToken);
+                        _logger.LogInformation("API Respondeu com sucesso.");
+                    }
+                    else
+                    {
+                        _logger.LogWarning("API retornou erro: {statusCode}", response.StatusCode);
+                    }
+                }                
+                catch (Exception ex)
                 {
-                    logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    _logger.LogError("Erro ao acessar API: {message}", ex.Message);
                 }
-                await Task.Delay(1000, stoppingToken);
+
+                await Task.Delay(120000, stoppingToken);
             }
         }
     }
