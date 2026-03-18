88ce020923ae20c9bd279bbae9d63a1d15c1319c
EricoOliveiraGomes <gomeserico78@gmail.com>
2026-03-18 08:25:17 -0300

    refatorações

--- Worker.cs
+++ Worker.cs
@@ -1,7 +1,5 @@
 using Microsoft.Data.Sqlite;
-using SQLitePCL;
 using System.Data;
-using System.Data.Common;
 
 namespace OmdbWorkerService
 {
@@ -9,18 +7,18 @@ namespace OmdbWorkerService
     {
         private readonly ILogger<Worker> _logger;
         private readonly HttpClient _httpClient;
-        private static Random random = new();
-        //private readonly IDbConnection _connection;
+        private static Random random = new();        
         private readonly string _connectionString;
         private readonly IConfiguration _configuration;
+        private readonly string _baseUri;
 
-        public Worker(ILogger<Worker> logger, IHttpClientFactory httpClientFactory, IConfiguration configuration)//, IDbConnection connection)
-        {
+        public Worker(ILogger<Worker> logger, IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        { 
             _logger = logger;            
-            _httpClient = httpClientFactory.CreateClient();
-            //_connection = connection;
+            _httpClient = httpClientFactory.CreateClient();            
             _configuration = configuration;
-            _connectionString = _configuration.GetConnectionString("SqliteConnection");
+            _connectionString = _configuration.GetConnectionString("SqliteConnection")!;            
+            _baseUri = _configuration.GetValue<string>("BaseUri")!;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -35,35 +33,14 @@ namespace OmdbWorkerService
                     char titulo = ObterLetraAleatoria();
                     int pagina = ObterPaginaAleatoria();
                     int ano = ObterAnoAleatorio();
-                    //var connectionString = "Data Source=omdb.db";
-
-                    string requestUri = $"http://www.omdbapi.com/?apikey=b0dd6377&plot=full&t={titulo}&page={pagina}&y={ano}";
+                    
+                    string requestUri = $"{_baseUri}&t={titulo}&page={pagina}&y={ano}";
 
                     var response = await _httpClient.GetAsync(requestUri, stoppingToken);
 
                     if (response.IsSuccessStatusCode)
                     {
                         var conteudo = await response.Content.ReadAsStringAsync(stoppingToken);
-
-                        //SQLitePCL.raw.SetProvider(new SQLite3Provider_e_sqlite3());
-                        //SQLitePCL.raw.SetProvider(new SQLite3Provider_e_sqlite3());
-                        //SQLitePCL.Batteries.Init(); 
-                        //SQLitePCL.Batteries_V2.Init();
-                        //SQLitePCL.
-                        //SQLitePCL
-
-
-                        //var sqlInsert = "INSERT INTO Produtos (Nome, Preco) VALUES (@Nome, @Preco)";
-                        //var novoProduto = new { Nome = "Notebook", Preco = 3500.00 };
-                        //_connection.Execute(sqlInsert, novoProduto);
-
-                        //_connection.Open();
-                        //var insertCmd = _connection.CreateCommand();
-                        //insertCmd.CommandText = "INSERT INTO Omdb (InformacaoFilme) VALUES (@InformacaoFilme)";
-                        //var novaInformacaoFilme = new { InformacaoFilme = conteudo };
-                        ////insertCmd.Parameters.Add("$conteudo", conteudo);
-                        //insertCmd.ExecuteNonQuery();
-
 
                         using (IDbConnection db = new SqliteConnection(_connectionString))
                         {                            
@@ -81,19 +58,7 @@ namespace OmdbWorkerService
                                 insertCmd.Parameters.Add(new SqliteParameter("@InformacaoFilme", conteudo));
                                 insertCmd.ExecuteNonQuery();
                             }
-
-                            //var novaInformacaoFilme = new { InformacaoFilme = conteudo };
-                            //insertCmd.Parameters.Add(conteudo);
-                            
-
-                            //var novaLog = new { Msg = "Worker ativo", Date = DateTime.Now };
-                            //var sql = "INSERT INTO Logs (Message, CreatedAt) VALUES (@Msg, @Date)";
-
-                            //await db.ExecuteAsyn(sql, novaLog);
-                            //_logger.LogInformation("Log inserido com sucesso.");
-                        } // Conexão fechada e liberada aqui (Dispose)
-
-
+                        } 
 
                         _logger.LogInformation("API Respondeu com sucesso.");
                     }
